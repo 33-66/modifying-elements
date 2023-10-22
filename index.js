f4a30cc989e2e7fc2842ced9b72c0613c0c982c7
33-66 <simon.nganga@student.moringaschool.com>
2023-10-22 16:15:00 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const nan = document.querySelector('#main')
+main.remove(nan)
+
+const newHeader = document.createElement('h1')
+newHeader.id ='victory'
+// document.body.append(newHeader)
+
+newHeader.textContent = 'YOUR-NAME is the champion';
+    document.body.appendChild(newHeader);
+
